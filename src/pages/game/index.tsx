ba9998e6df9ba3766253a83b1c71a9f84258191b
Yuki Takubo <takubo19108@gmail.com>
2025-02-09 01:29:55 +0900

    feat: /game カウントダウン機能

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 import { Theme, Box, Text } from "@radix-ui/themes";
 import styles from "./styles.module.scss";
 import { use } from "react";
@@ -8,11 +9,27 @@ function Game() {
   const router = useRouter();
   // ルートからのデータをクエリで取得
   const { numOfPlayers, isMuted } = router.query;
+
+  // タイマーのカウントダウン処理
+  const [count, setCount] = useState(10);
+  useEffect(() => {
+    if(count > 0){
+      const timer = setInterval(() => {
+        setCount((prev) => prev - 1);
+      }, 1000);
+      return () => clearInterval(timer);
+    }
+  }, [count]);
+
+
+  // 妨害演出
+
   return (
     <Theme>
       <Box>
         <Text>プレイヤー数:{numOfPlayers}</Text>
         <Text>ミュート:{isMuted === "true" ? "オン" : "オフ"}</Text>
+        <Text>残り時間:{count}</Text>
       </Box>
     </Theme>
   );
