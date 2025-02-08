7df6d19e1df2d1d173b1cf6666488e25a3bc676e
Yuki Takubo <takubo19108@gmail.com>
2025-02-09 01:29:00 +0900

    feat: /game ルートページの入力内容を受け取って表示させる

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { Theme, Box, Text } from "@radix-ui/themes";
 import styles from "./styles.module.scss";
+import { use } from "react";
 
 function Game() {
+  const router = useRouter();
+  // ルートからのデータをクエリで取得
+  const { numOfPlayers, isMuted } = router.query;
   return (
-    <div>
-      
-    </div>
+    <Theme>
+      <Box>
+        <Text>プレイヤー数:{numOfPlayers}</Text>
+        <Text>ミュート:{isMuted === "true" ? "オン" : "オフ"}</Text>
+      </Box>
+    </Theme>
   );
 }
 
