f62ca3f6cf45e6b794ee94cff0857b896f960713
Yuki Takubo <takubo19108@gmail.com>
2025-02-08 11:24:50 +0900

    update: 一旦startページにリダイレクトされないようにする

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
-  // ルートページを/startにリダイレクトする
-  async redirects(){
-    return[
-      {
-        source: "/",
-        destination: "/start",
-        permanent: true,
-      },
-    ];
-  },
-
+  // URIを開いたらpages/index.tsxを表示する
   // React Strict Modeを有効にする
   reactStrictMode: true,  
 };
