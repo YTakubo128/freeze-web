a25b0937f516e0bb33095c47f131f769b335b673
Yuki Takubo <takubo19108@gmail.com>
2025-02-08 16:00:22 +0900

    add: ルートページ import周り

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,11 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { useForm} from "react-hook-form";
+import { Theme, Box, Flex, Text, Button } from "@radix-ui/themes";
+import { PlusIcon, MinusIcon, SpeakerLoudIcon, SpeakerOffIcon } from "@radix-ui/react-icons";
+import "@radix-ui/themes/styles.css";
+
+import style from "./styles.module.scss";
 
 
 function Home() {
