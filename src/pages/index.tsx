d4ff38854f2130f38737a6d037b16d73cf00caf8
Yuki Takubo <takubo19108@gmail.com>
2025-02-08 16:03:49 +0900

    feat: プレイ人数の変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,20 @@ import style from "./styles.module.scss";
 function Home() {
   const { register, setValue, watch, handleSubmit} = useForm({
     defaultValues: {
+      numOfPlayers: 4,
       isMuted: false,
     },
   });
+
+  //プレイヤー数の増減
+  const numOfPlayers = watch("numOfPlayers");
+  const incrementPlayers = () => {
+    if(numOfPlayers < 4) setValue("numOfPlayers", numOfPlayers + 1);
+  };
+
+  const decrementPlayers = () => {
+    if(numOfPlayers > 1) setValue("numOfPlayers", numOfPlayers - 1);
+  };
   //音量調整
   const isMuted = watch("isMuted");
   const toggleMute = () => setValue("isMuted", !isMuted);
@@ -23,6 +34,14 @@ function Home() {
         <Button onClick={toggleMute}>
           { isMuted ? <SpeakerOffIcon/> : <SpeakerLoudIcon/> }
         </Button>
+      </Box>
+      <Box>
+        <Text>プレイ人数を選択してください</Text>
+        <Flex>
+          <Button onClick={decrementPlayers}><MinusIcon/></Button>
+          <Text>{numOfPlayers}</Text>
+          <Button onClick={incrementPlayers}><PlusIcon/></Button>
+        </Flex>
       </Box>
       
     </Theme>
