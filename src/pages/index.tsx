18e48ce50774629d207b58064b3abd85b916f993
Yuki Takubo <takubo19108@gmail.com>
2025-02-09 19:56:40 +0900

    chore: 気になった部分

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { Theme, Box, Flex, Text, Button } from "@radix-ui/themes";
 import { PlusIcon, MinusIcon, SpeakerLoudIcon, SpeakerOffIcon } from "@radix-ui/react-icons";
 import "@radix-ui/themes/styles.css";
