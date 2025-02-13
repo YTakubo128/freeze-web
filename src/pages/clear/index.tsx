a68e4f8eaf33e979e4c082c45453e98a5b70e18e
Yuki Takubo <takubo19108@gmail.com>
2025-02-13 14:44:10 +0900

    update: クリア画面戻るようにした

--- src/pages/clear/index.tsx
+++ src/pages/clear/index.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
+import { Theme, Box, Button } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import styles from "./styles.module.scss";
+import { useRouter } from 'next/router';
 
-function Clear() {
+const ClearPage = () => {
+  const router = useRouter();
+
   return (
-    <div>
-      
-    </div>
+    <Theme>
+      <Button onClick={() => router.push('/')}>スタートへ戻る</Button>
+    </Theme>
   );
-}
+};
 
-export default Clear;
+export default ClearPage;
