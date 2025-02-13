2cb79b14fe5e95bf170d573b5b3e1503bf53807c
Yuki Takubo <takubo19108@gmail.com>
2025-02-13 11:27:48 +0900

    feat: cssをリセットさせる

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps } from 'next/app';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './reset.min.css';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className={`${geistSans.variable} ${geistMono.variable} antialiased`} style={{ margin: 0 }}>
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default MyApp;
